a95da1ea3cbdebb02c6b949ef2f4ee3c74255832
Sachi Goel <67666178+computer-s-2@users.noreply.github.com>
2026-03-13 16:59:32 -0400

    added files

---
+++ spy_recipe.sh
@@ -0,0 +1,21 @@
+  #!/bin/bash
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./data/ \
+  --data_path spy.csv \
+  --model_id SPY_Optimized \
+  --model unitsf \
+  --data custom \
+  --features S \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --use_norm True \
+  --use_decomp True \
+  --emb_type patch \
+  --ff_type trans \
+  --train_epochs 5 \
+  --target SPY \
+  --freq d
